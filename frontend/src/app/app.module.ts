b6902020f08dfa2e991721d0c0799a8cde6e8c27
arulkumarkandasamy <karul43@yahoo.co.in>
2021-04-02 15:13:58 +0100

    securing Flask with Auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import * as Auth0 from 'auth0-web';
+import {CallbackComponent} from './callback.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppComponent} from './app.component';
+import {ExamsApiService} from './exams/exams-api.service';
+import {ExamFormComponent} from './exams/exam-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ExamsComponent} from './exams/exams.component';
+
+const appRoutes: Routes = [
+  { path: 'callback', component: CallbackComponent },
+  { path: 'new-exam', component: ExamFormComponent },
+  { path: '', component: ExamsComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamFormComponent,
+    ExamsComponent,
+    CallbackComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
-  providers: [],
+  providers: [ExamsApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    Auth0.configure({
+      domain: 'dev-csc9uct4.eu.auth0.com',
+      audience: 'https://online-exam.digituz.com.br',
+      clientID: 'YFG8j47jGHjqzKAX0oDj5TWMyWQBQqeM',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid profile manage:exams'
+    });
+  }
+}
