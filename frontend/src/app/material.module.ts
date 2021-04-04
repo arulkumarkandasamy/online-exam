e0f34f72e0e2187c09a4db6bf2f79a4b5a807f2a
arulkumarkandasamy <karul43@yahoo.co.in>
2021-04-04 10:01:00 +0100

    using Angular Material Cards and CSS Grid layout to show exams

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -5,6 +5,7 @@ import  {MatToolbarModule} from '@angular/material/toolbar';
 import  {MatIconModule} from '@angular/material/icon';
 import  {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatIconModule,
     MatListModule,
     MatButtonModule,
+    MatCardModule,
   ],
   exports: [
     MatSidenavModule,
@@ -21,6 +23,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatIconModule,
     MatListModule,
     MatButtonModule,
+    MatCardModule,
   ]
 })
 export class MaterialModule {}
